33f52b61a0150ebc83b64c269616e3828712fcee
edward.pinzon <edward.pinzon@GYG_INV_23>
2024-02-15 15:25:50 -0500

    Agregar archivos de proyecto.

---
+++ EjerciciosAPI/Controllers/ParImparController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NumeroParImparController : ControllerBase
+{
+    [HttpGet("Validar numero")]
+    public IActionResult ValidarNumero(int? numero)
+    {
+        if (numero.HasValue)
+        {
+            string resultado = (numero % 2 == 0) ? $"El número {numero} es par" : $"El número {numero} es impar";
+            return Ok(new { Resultado = resultado });
+        }
+        else
+        {
+            return BadRequest(new { Error = "Por favor, ingrese algún dato" });
+        }
+    }
+}
---
+++ EjerciciosAPI/Controllers/PromedioController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PromedioController : ControllerBase
+{
+    [HttpGet("Calcular promedio")]
+    public IActionResult CalcularPromedio(double numero1, double numero2, double numero3)
+    {
+        if (numero1 < 0 || numero2 < 0 || numero3 < 0)
+        {
+            return BadRequest(new { Error = "Por favor, ingrese números enteros positivos" });
+        }
+        else
+        {
+            double promedio = (numero1 + numero2 + numero3) / 3;
+            return Ok(new { Promedio = promedio });
+        }
+    }
+}
